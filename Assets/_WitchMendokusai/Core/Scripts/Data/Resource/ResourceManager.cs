01700d5cee29d28eebd5553cfbedd7e3e813b710
KarmoDDrine <mascari4615@gmail.com>
2025-07-24 19:54:01 +0900

    ResourceManager 추가

---
+++ Assets/_WitchMendokusai/Core/Scripts/Data/Resource/ResourceManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WitchMendokusai
+{
+	[CreateAssetMenu(fileName = nameof(ResourceManager), menuName = "ResourceManager")]
+	public class ResourceManager : ScriptableObject
+	{
+		[NonSerialized] private static ResourceManager instance;
+		public static ResourceManager Instance
+		{
+			get
+			{
+				if (instance == null)
+					instance = Resources.Load(typeof(ResourceManager).Name) as ResourceManager;
+
+				return instance;
+			}
+			private set => instance = value;
+		}
+
+		[field: SerializeField] public GameObject EXPPrefab { get; private set; } = null;
+		[field: SerializeField] public GameObject LootItemPrefab { get; private set; } = null;
+	}
+}
