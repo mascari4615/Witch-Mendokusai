6c7d4566d1d0b807f7dfe897fc1d16a09d089d3a
KarmoDDrine <mascari4615@gmail.com>
2025-08-02 16:49:21 +0900

    UI DungeonEntrance (WIP)

--- Assets/_WitchMendokusai/Content/Explorer/Dungeon/DungeonEntrance/Scripts/UIDungeonEntrance.cs
+++ Assets/_WitchMendokusai/Content/Explorer/Dungeon/DungeonEntrance/Scripts/UIDungeonEntrance.cs
@@ -9,11 +9,12 @@ namespace WitchMendokusai
 {
 	public class UIDungeonEntrance : UIPanel
 	{
-		[SerializeField] private Transform dungeonSelectButtonParent;
-		[SerializeField] private UISlot dungeonSlot;
+		[SerializeField] private Transform dungeonSelectButtonsParent;
+		[SerializeField] private GameObject dungeonSelectButtonPrefab;
 		[SerializeField] private Button dungeonEnterButton;
+		[SerializeField] private ToolTip toolTip;
+		protected List<UISlot> dungeonSelectButtons;
 
-		private List<UISlot> dungeonSelectButtons;
 		private UIRewards rewardUI;
 		private UIDungeonConstraint constraintUI;
 
@@ -24,23 +25,31 @@ namespace WitchMendokusai
 
 		public override void Init()
 		{
-			dungeonSelectButtons = dungeonSelectButtonParent.GetComponentsInChildren<UISlot>(true).ToList();
-
-			for (int i = 0; i < dungeonSelectButtons.Count; i++)
-			{
-				dungeonSelectButtons[i].SetSlotIndex(i);
-				dungeonSelectButtons[i].Init();
-				dungeonSelectButtons[i].SetClickAction(slot => SelectDungeon(slot.Index));
-			}
-
 			rewardUI = GetComponentInChildren<UIRewards>(true);
 			rewardUI.Init();
 
 			constraintUI = GetComponentInChildren<UIDungeonConstraint>(true);
 			constraintUI.Init();
 
-			dungeonSlot.Init();
+			dungeonSelectButtons = new List<UISlot>();
+			for (int i = 0; i < 10; i++) // TODO: Magic Number
+			{
+				GameObject buttonInstance = Instantiate(dungeonSelectButtonPrefab, dungeonSelectButtonsParent);
+				dungeonSelectButtons.Add(buttonInstance.GetComponent<UISlot>());
+				dungeonSelectButtons[i].SetSlotIndex(i);
+				dungeonSelectButtons[i].Init();
+				dungeonSelectButtons[i].SetClickAction((slot) => { SelectDungeon(slot.Index); });
+				dungeonSelectButtons[i].gameObject.SetActive(false);
+			}
 
+			dungeonEnterButton.navigation = new Navigation
+			{
+				mode = Navigation.Mode.Explicit,
+				selectOnUp = null,
+				selectOnDown = null,
+				selectOnLeft = dungeonSelectButtons.First().Selectable,
+				selectOnRight = null
+			};
 			dungeonEnterButton.onClick.AddListener(EnterTheDungeon);
 		}
 
@@ -51,17 +60,24 @@ namespace WitchMendokusai
 			if (dungeons == null || dungeons.Count == 0)
 				Debug.LogError("No Dungeon Data");
 
-			dungeonSelectButtons.First().Select(); // 첫 번째 던전에 UI Select Focus 주기 - 250315. 19:20
-		}
-
-		public override void UpdateUI()
-		{
+			int maxButtonIndex = Mathf.Max(dungeonSelectButtons.Count - 1, dungeons.Count - 1);
 			for (int i = 0; i < dungeonSelectButtons.Count; i++)
 			{
 				if (i < dungeons.Count)
 				{
 					dungeonSelectButtons[i].SetSlot(dungeons[i]);
 					dungeonSelectButtons[i].gameObject.SetActive(true);
+
+					dungeonSelectButtons[i].SetNavigation(
+						new Navigation
+						{
+							mode = Navigation.Mode.Explicit,
+							selectOnUp = (i > 0) ? dungeonSelectButtons[i - 1].Selectable : null,
+							selectOnDown = (i < maxButtonIndex) ? dungeonSelectButtons[i + 1].Selectable : null,
+							selectOnLeft = null,
+							selectOnRight = null
+						}
+					);
 				}
 				else
 				{
@@ -69,18 +85,31 @@ namespace WitchMendokusai
 				}
 			}
 
+			dungeonSelectButtons.First().Select(); // 첫 번째 던전에 UI Select Focus 주기 - 250315. 19:20
+		}
+
+		protected override void OnOpen()
+		{
 			SelectDungeon(0);
+		}
+
+		public override void UpdateUI()
+		{
+			UpdateTooltip();
 		}
 
 		public void SelectDungeon(int index)
 		{
 			curDungeonIndex = index;
-			UpdateDungeonPanel();
+			UpdateTooltip();
+
+			dungeonEnterButton.Select();
 		}
 
-		private void UpdateDungeonPanel()
+		private void UpdateTooltip()
 		{
-			dungeonSlot.SetSlot(CurDungeon);
+			toolTip.SetToolTipContent(dungeonSelectButtons[curDungeonIndex].Data);
+
 			rewardUI.UpdateUI(CurDungeon.Rewards);
 
 			constraintUI.SetDungeon(CurDungeon);
