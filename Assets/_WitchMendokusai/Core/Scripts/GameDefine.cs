9b75923835e1dbac401c1afd8fb90651422b8c19
KarmoDDrine <mascari4615@gmail.com>
2025-08-02 16:49:48 +0900

    chore

--- Assets/_WitchMendokusai/Core/Scripts/GameDefine.cs
+++ Assets/_WitchMendokusai/Core/Scripts/GameDefine.cs
@@ -9,7 +9,7 @@ namespace WitchMendokusai
 	public static class GameDefine
 	{
 		public const float HealPercentage = 0.5f;
-		public const float MagnetPercentage = 1;
+		public const float MagnetPercentage = 0.25f;
 		public const float LootItemSpawnPosOffsetXZ = 0.3f;
 		public const float LootItemSpawnPosOffsetY = 0.3f;
 	}
